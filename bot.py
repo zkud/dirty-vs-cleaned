4685c5a46d1e8452d82a918b31c9424699eb9c1c
Anton Kudyrka <33183682+L1ttl3S1st3r@users.noreply.github.com>
2019-11-19 12:00:34 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,42 @@
+""" Код простенького телеграм - бота, который уведомляет о результатах обучения """
+import requests  
+import datetime
+
+
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
+
+
+def notify(message: str, token: str, owner_chat_id: int):       
+    """ sends message 'message' to owner """
+    print("sending message to owner")
+
+    notify_bot = BotHandler(token)
+    return notify_bot.send_message(owner_chat_id, message)
+
